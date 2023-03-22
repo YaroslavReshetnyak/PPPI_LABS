8dc24e233a73273f8ae0911caaf7788ad58e38fa
Yaroslav Ovsyannikov <yaroslav.ovsyannikov@1win.pro>
2023-03-22 23:29:21 +0300

    little fixes

--- program/cardImages.java
+++ program/cardImages.java
@@ -32,11 +32,7 @@ public final class CardImages
 	private static Image getCard( String pCode )
 	{
 		Image image = (Image) aCards.get( pCode );
-		if( image == null )
-		{
-			image = new Image(CardImages.class.getClassLoader().getResourceAsStream( IMAGE_LOCATION + pCode + IMAGE_SUFFIX ));
-			aCards.put( pCode, image );
-		}
+
 		return image;
 	}
 	
